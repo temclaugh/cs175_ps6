96571f77e6534ae81ed20a2ec7f3c21c1e2fbde3
joseph booth <jo.m.booth@gmail.com>
2014-10-30 21:35:26 -0400

    di ei etc

--- asst6.cpp
+++ asst6.cpp
@@ -330,9 +330,6 @@ static Cvec3 lerp(Cvec3 src, Cvec3 dest, float alpha);
 static Quat cond_neg(Quat q);
 static Quat qpow(Quat q, float alpha);
 
-/* Cvec3 bezierInterpolation(Cvec3 c1, Cvec3 c2, Cvec3, d, Cvec3 e) { */
-
-/* } */
 
 Cvec3 getDTrans(Cvec3 c_i_1, Cvec3 c_i_neg_1, Cvec3 c_i) {
   return (c_i_1 - c_i_neg_1)/6 + c_i;
@@ -342,12 +339,39 @@ Cvec3 getETrans(Cvec3 c_i_2, Cvec3 c_i_1, Cvec3 c_i) {
   return (c_i_2 - c_i)/-6 + c_i_1;
 }
 
+bezierTrans(Cvec3 c_i_neg_1, Cvec3 c_i, Cvec3 c_i_1, Cvec3 c_i_2, int i, float t) {
+
+  Cvec3 d = getDTrans(c_i_1, c_i_neg_1, c_i);
+  Cvec3 e = getETrans(c_i_2, c_i_1, c_i);
+
+  Cvec3 f = (1 - t + i)*c_i + (t - i)d;
+  Cvec3 g = (1 - t + i)*d + (t - i)*e;
+  Cvec3 h = (1 - t + i)*e + (t - i)*c_i_1;
+  Cvec3 m = (1 - t + i)*f + (t - i)*g;
+  Cvec3 n = (1 - t + i)*g + (t - i)*h;
+  return (1 - t + i)*m + (t - i)*n;
+}
+
 Quat getDRot(Quat c_i_1, Quat c_i_neg_1, Quat c_i) {
   return qpow(cond_neg(c_i_1 * inv(c_i_neg_1)), 1.0/6.0) * c_i;
 }
 
 Quat getERot(Quat c_i_2, Quat c_i_1, Quat c_i) {
   return qpow(cond_neg(c_i_2 * inv(c_i)), -1.0/6.0) * c_i_1;
+}
+
+Quat bezierRot(Quat c_i_neg_1, Quat c_i, Quat c_i_1, Quat c_i_2, int i, float t) {
+
+  Quat d = getDRot(c_i_1, c_i_neg_1, c_i);
+  Quat e = getERot(c_i_2, c_i_1, c_i);
+
+  Quat f = qpow(cond_neg(c_i), 1 - t + i) * qpow(cond_neg(d), t -i);
+  Quat g = qpow(cond_neg(d), 1 - t + i) * qpow(cond_neg(e), t - i);
+  Quat h = qpow(cond_neg(e), 1 - t + i) * qpow(cond_neg(c_i_1), t - i);
+  Quat m = qpow(cond_neg(f), 1 - t + i) * qpow(cond_neg(g), t - i);
+  Quat n = qpow(cond_neg(g), 1 - t + i) * qpow(cond_neg(h), t - i);
+
+  return qpow(cond_neg(m), 1 - t + i) + qpow(cond_neg(n), t - i);
 }
 
 bool interpolateAndDisplay(float t) {
